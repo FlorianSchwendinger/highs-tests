f25944f3f5a2a49c89334f11c8492da303cf0ea4
Florian Schwendinger <Florian_Schwendinger@gmx.at>
2025-01-21 10:11:42 +0100

    change output dir

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 prob_dir="./lp"
-out_dir="./out_$(date +"%Y-%m-%d")"
+highs_output_dir="$HOME/highs-output"
 highs="$HOME/highslib/bin/highs"
 
-mkdir -p $out_dir
+mkdir -p $highs_output_dir
 
 for file_path in "$prob_dir"/*
 do
   if [ -f "$file_path" ]; then
     file_name=$(basename "$file_path")
     echo "Run $file_path"
-    $highs $file_path &> $out_dir/$file_name.out
+    $highs $file_path &> $highs_output_dir/$file_name.out
   fi
 done
